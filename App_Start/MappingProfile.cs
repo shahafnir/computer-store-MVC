b3590766d084a8ae66a4ae8e40f4ff313b956710
Nir Shahaf <nirshahaf1@gmail.com>
2021-06-13 12:49:01 +0300

    Adding MappingProfile

---
+++ App_Start/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using computer_store_MVC.Models;
+using computer_store_MVC.Dtos;
+
+namespace computer_store_MVC.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Computer, ComputerDto>();
+            Mapper.CreateMap<ComputerDto, Computer>();
+        }
+    }
+}
